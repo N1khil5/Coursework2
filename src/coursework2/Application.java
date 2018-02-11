7837bc26860ec9791abab4692e043a6c24928557
Nikhil <Nikhil@192.168.20.65>
2018-02-11 10:44:01 +0400

    Added functionality to menu on Application class. Bank Account and User classes has been created. Bank Account class still requires functionality for deposit and withdrawals.

--- src/coursework2/Application.java
+++ src/coursework2/Application.java
@@ -7,19 +7,20 @@ package coursework2;
 
 import java.util.Scanner;
 
-/** 
+/**
  *
  * @author Nikhil
  */
 public class Application {
 
     static BankAccount bankAccount;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         System.out.println("Welcome to the Breaking Bad Bank.");
         System.out.println("Please select a number corresponding to the choice you wish to make.");
         while (true) {
@@ -36,6 +37,27 @@ public class Application {
                 Scanner s = new Scanner(System.in);
                 option = s.nextInt();
 
+                User Saul = new User("Saul", "Goodman", bankAccount);
+                double[] transactionSaul = {50, 10, -20, 10, -20, 20, 10, 50, -10, 10, -10, 50};
+                for (int i = 0; i < transactionSaul.length; i++) {
+                    Saul.addTransaction(transactionSaul[i]);
+                }
+                User Walter = new User("Walter", "White", bankAccount);
+                double[] transactionWalter = {20, 20, -20, 50, -20, 10, 50, 50, -20, 10, 10};
+                for (int i = 0; i < transactionWalter.length; i++) {
+                    Walter.addTransaction(transactionWalter[i]);
+                }
+                User Jessie = new User("Jessie", "Pinkman", bankAccount);
+                double[] transactionJessie = {50, 10, 10, -10, -10, 50, 20, -10, -20};
+                for (int i = 0; i < transactionJessie.length; i++) {
+                    Jessie.addTransaction(transactionJessie[i]);
+                }
+                User Hank = new User("Hank", "Schrader", bankAccount);
+                double[] transactionHank = {50, 10, -20, 20, 10, -20};
+                for (int i = 0; i < transactionHank.length; i++) {
+                    Hank.addTransaction(transactionHank[i]);
+                }
+
                 switch (option) {
                     case 1:
                         System.out.println("You have selected Create Bank Account.");
@@ -47,42 +69,37 @@ public class Application {
                         System.out.println("Your account balance is " + accountBalance + "");
                         System.out.println("Do you want to use the default users?");
                         String answer = s.next();
-                        if (answer.equals("y"))
-                        {
-                            User Saul = new User ("Saul","Goodman",bankAccount);
-                            double[] transactionSaul = {50,10,-20,10,-20,20,10,50,-10,10,-10,50};
-                            for (int i =0; i < transactionSaul.length; i++)
-                            {
+                        if (answer.equals("y")) {/*
+                            User Saul = new User("Saul", "Goodman", bankAccount);
+                            double[] transactionSaul = {50, 10, -20, 10, -20, 20, 10, 50, -10, 10, -10, 50};
+                            for (int i = 0; i < transactionSaul.length; i++) {
                                 Saul.addTransaction(transactionSaul[i]);
                             }
-                            User Walter = new User ("Walter","White",bankAccount);
-                            double[] transactionWalter = {20,20,-20,50,-20,10,50,50,-20,10,10};
-                            for (int i =0; i < transactionWalter.length; i++)
-                            {
+                            User Walter = new User("Walter", "White", bankAccount);
+                            double[] transactionWalter = {20, 20, -20, 50, -20, 10, 50, 50, -20, 10, 10};
+                            for (int i = 0; i < transactionWalter.length; i++) {
                                 Walter.addTransaction(transactionWalter[i]);
                             }
-                            User Jessie = new User ("Jessie","Pinkman",bankAccount);
-                            double[] transactionJessie = {50,10,10,-10,-10,50,20,-10,-20};
-                            for (int i =0; i < transactionJessie.length; i++)
-                            {
+                            User Jessie = new User("Jessie", "Pinkman", bankAccount);
+                            double[] transactionJessie = {50, 10, 10, -10, -10, 50, 20, -10, -20};
+                            for (int i = 0; i < transactionJessie.length; i++) {
                                 Jessie.addTransaction(transactionJessie[i]);
-                            }    
-                            User Hank = new User ("Hank","Schrader",bankAccount);
-                            double[] transactionHank = {50,10,-20,20,10,-20};
-                            for (int i =0; i < transactionHank.length; i++)
-                            {
-                                Hank.addTransaction(transactionHank[i]);
                             }
+                            User Hank = new User("Hank", "Schrader", bankAccount);
+                            double[] transactionHank = {50, 10, -20, 20, 10, -20};
+                            for (int i = 0; i < transactionHank.length; i++) {
+                                Hank.addTransaction(transactionHank[i]);
+                            } */
                             continue;
-                        }
-                        else
+                        } else {
                             System.out.println("What would you like to name the user?");
-                            String name = s.next();
-                            System.out.println("Welcome to Breaking Bad Bank, " + name + "!");
-                            System.out.println("Please enter your last name");
-                            String surname = s.next();
-                            System.out.println("Thank you for banking with us, " + name + " " + surname + "!");
-                            
+                        }
+                        String name = s.next();
+                        System.out.println("Welcome to Breaking Bad Bank, " + name + "!");
+                        System.out.println("Please enter your last name");
+                        String surname = s.next();
+                        System.out.println("Thank you for banking with us, " + name + " " + surname + "!");
+
                         break;
                     case 2:
                         System.out.println("You have selected Create User.");
@@ -93,6 +110,10 @@ public class Application {
                         break;
                     case 3:
                         System.out.println("You have selected Run Simulation");
+                        Walter.start();
+                        Saul.start();
+                        Jessie.start();
+                        Hank.start();
                         break;
                     case 4:
                         System.out.println();
